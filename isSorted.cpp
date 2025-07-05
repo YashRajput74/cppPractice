8299b3113f57a9bfe5ed3d7ee4d72b638ec17602
Yash Rajput <yashrajput7455@gmail.com>
2025-07-05 21:21:06 +0530

    is sorted in ascending order

---
+++ isSorted.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the no of elements:";
+    cin>>n;
+    int arr[n];
+    int size=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    int isSorted;
+    for(int i=1;i<size;i++){
+        if(arr[i-1]>arr[i]){
+            isSorted=false;
+            break;
+        }
+    }
+    if(isSorted){
+        cout<<"Array is sorted";
+    }
+    else{
+        cout<<"Array is not sorted";
+    }
+    return 0;
+}
