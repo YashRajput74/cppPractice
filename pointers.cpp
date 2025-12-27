984e73c3cee789ccfebdca7ed783db95cf03f594
Yash Rajput <yashrajput7455@gmail.com>
2025-12-27 22:48:01 +0530

    basic error solved

--- pointers.cpp
+++ pointers.cpp
@@ -11,14 +11,4 @@ int main(){
     cout << p <<endl;//address of p
     cout << *q <<endl;//address of p
 }
-
-void ChangeA(int a){
-    int a=20;
-}
-
-int main(){
-    int a=10;
-    ChangeA(a);
-    cout << "inside max fn: "<<a <<endl;
-    return 0;
-}
+/* a file can not have two mains... */
